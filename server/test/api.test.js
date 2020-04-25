0e772f0dc039dac2b5de6d3e83820a5b6760ed59
Cezara <cezaraluca@yahoo.com>
2020-04-25 16:24:40 +0300

    test failed mocha

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -20,21 +20,49 @@ describe("GET /api/v1", () => {
 
 describe("POST /api/v1/messages", () => {
   it("responds with inserted msg", (done) => {
-    const result = {
+    const requestObj = {
       name: "Cezara",
       message: "cea mai aplicatzie",
       latitude: -90,
       longitude: 180,
     };
+    const responseObj = {
+      ...requestObj,
+      _id: "5b57d127923211248855977c",
+      date: new Date(),
+    };
     request(app)
       .post("/api/v1/messages")
-      .send(result)
+      .send(requestObj)
       .set("Accept", "application/json")
       .expect("Content-Type", /json/)
-      .expect(200, result, done)
-      .then((response) => {
-        console.log(response);
-        done();
-      });
+      .expect((res) => {
+        res.body._id = "5b57d127923211248855977c";
+        res.body.date = new Date();
+      })
+      .expect(200, responseObj, done);
+  });
+  it("can sign up with a name that has diacritics", (done) => {
+    const requestObj = {
+      name: "Yo",
+      message: "cea mai aplicatzie",
+      latitude: -90,
+      longitude: 180,
+    };
+    const responseObj = {
+      ...requestObj,
+      _id: "5b57d127923211248855977c",
+      date: new Date(),
+    };
+    request(app)
+      .post("/api/v1/messages")
+      .send(requestObj)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect((res) => {
+        res.body._id = "5b57d127923211248855977c";
+        res.body.date = new Date();
+      })
+      .expect(200, responseObj, done);
   });
 });
