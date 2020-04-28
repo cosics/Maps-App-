16272084cb3d954a70d34419cbcdbbf3596b39f0
Cezara <cezaraluca@yahoo.com>
2020-04-29 00:02:07 +0300

    FINAL before deploy

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
 
 import "leaflet/dist/leaflet.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
