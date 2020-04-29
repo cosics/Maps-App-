59f68582396642f6632243930a0218fc736b2bb7
Cezara <cezaraluca@yahoo.com>
2020-04-29 14:14:29 +0300

    Adding  to source control

---
+++ client/build/precache-manifest.3fe082bd938354bc350f89ec5e123e3d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14eed9c38094107c5b54b374310c8be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfa5cd14211e15c74aa8",
+    "url": "/static/css/2.a0f07c34.chunk.css"
+  },
+  {
+    "revision": "235480ae4a56a89ce38e",
+    "url": "/static/css/main.f6f90aba.chunk.css"
+  },
+  {
+    "revision": "cfa5cd14211e15c74aa8",
+    "url": "/static/js/2.2e5aeca7.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/static/js/2.2e5aeca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235480ae4a56a89ce38e",
+    "url": "/static/js/main.50114ae3.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "3fd7abd3ef70897e80e7532d58658bdd",
+    "url": "/static/media/message_location.3fd7abd3.svg"
+  },
+  {
+    "revision": "ff1c0bff277ac04b8d1f4734e054d994",
+    "url": "/static/media/user_location.ff1c0bff.svg"
+  }
+]);
